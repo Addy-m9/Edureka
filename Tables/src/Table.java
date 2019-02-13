e91684be3483f6cd1243d7d7b93b71e7a5f8363c
adarshkumarmallampeta <adarshkumarmallampeta@adarshs-mbp>
2019-02-12 23:09:36 -0500

    first commit

---
+++ Tables/src/Table.java
@@ -0,0 +1,24 @@
+
+public class Table {
+
+	public static void main(String [] args)
+	{
+		
+	System.out.println("Multiples of 10 are shown below");
+	
+int i =10;
+
+for(int n=0;n<=10;n++)
+{
+
+
+int x=i*n;
+
+System.out.println(x);
+}
+
+
+}
+}
+
+
